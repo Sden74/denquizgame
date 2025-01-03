2aa68dd1ce914b226fece04f4edc5a409f9b7d23
Sden74 <sitnov.den74@yandex.ru>
2025-01-03 16:42:41 +0500

    QG-03 XML korrect color & clickSecondChoiсe & CONTEXT description in MainActivity.kt below

--- app/src/main/java/com/example/denquizgame/MainActivity.kt
+++ app/src/main/java/com/example/denquizgame/MainActivity.kt
@@ -18,3 +18,70 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+/*
+interface Context {
+    fun getString()
+
+    class ContextImpl:Context{
+
+        override fun getString() {
+            TODO("real code to get string resource")
+        }
+
+        fun setDisplay(double: Any){
+
+        }
+
+    }
+    class Activity : Context {
+
+        private lateinit var baseContext: Context
+
+        fun setContext(context: Context) {
+            this.context = context
+        }
+
+        override fun getString() {
+            context.getString()
+        }
+
+    }
+
+    class Application:Context{
+        private lateinit var context: Context
+        private lateinit var application: Application
+
+        fun setContext(context: Context){
+                this.context = context
+        }
+
+        fun setApplication(application: Application){
+            this.application=application
+        }
+        override fun getString() {
+            context.getString()
+        }
+
+    }
+}
+
+class Init{
+    fun main(){
+        val app=makeApplication()
+        app.SetContext(Context.ContextImpl().apply {
+            setType(ApplicationTypeContext)
+        })
+
+        val activity=makeActivity()
+        activity.setContext(Context.ContextImpl().apply{
+            setDisplay("display")
+            setType(ActivityContextType)
+        })
+
+        activity.setApplication(app)
+
+        activity.callOnCreate()
+    }
+
+}*/
