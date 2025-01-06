f92d22e6dbc3bd7f4366a92bbc879fd52b37177c
Sden74 <sitnov.den74@yandex.ru>
2025-01-06 16:24:02 +0500

    QG-06 UiStates and GameRepository.Base

--- app/src/main/java/com/example/denquizgame/GameRepository.kt
+++ app/src/main/java/com/example/denquizgame/GameRepository.kt
@@ -6,4 +6,54 @@ interface GameRepository {
     fun saveUserChoice(index: Int)
     fun check(): CorrectAndUserChoiceIndexes
     fun next()
+
+    // Хранилище данных репозитория(два QuestionAndChoices)
+    class Base(
+        private val list: List<QuestionAndChoices> = listOf(
+            QuestionAndChoices(
+                question = "What color is the sky?",
+                choices = listOf("blue", "green", "red", "yellow"),
+                correctIndex = 0
+            ),
+            QuestionAndChoices(
+                question = "What color is the grass?",
+                choices = listOf("green", "blue", "red", "yellow"),
+                correctIndex = 0
+            )
+        )
+
+    ) : GameRepository {
+
+
+        private var index = 0
+
+        // 1  Возврат обертки (questionAndChoices) вопроса и списка ответов по индексу от пользователя
+        override fun questionAndChoices(): QuestionAndChoices {
+            return list[index]
+        }
+
+        private var userChoiceIndex = -1
+
+        // Храним в репе юзерский выбор
+        override fun saveUserChoice(index: Int) {
+            userChoiceIndex = index
+        }
+
+        // чек вернет обертку (CorrectAndUserChoiceIndexes) из юзерского и корректного индексов
+        override fun check(): CorrectAndUserChoiceIndexes {
+            return CorrectAndUserChoiceIndexes(
+                correctIndex = questionAndChoices().correctIndex,
+                userChoiceIndex = userChoiceIndex
+            )
+        }
+
+        override fun next() {
+            userChoiceIndex = -1
+
+            if (index + 1 == list.size)
+                index = 0
+            else
+                index++
+        }
+    }
 }
--- app/src/main/java/com/example/denquizgame/MainActivity.kt
+++ app/src/main/java/com/example/denquizgame/MainActivity.kt
@@ -12,32 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout)) { v, insets ->
+
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(/*findViewById(R.id.rootLayout)*/ binding.rootLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val viewModel: GameViewModel = GameViewModel(object : GameRepository {
-            override fun questionAndChoices(): QuestionAndChoices {
-                TODO("Not yet implemented")
-            }
-
-            override fun saveUserChoice(index: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun check(): CorrectAndUserChoiceIndexes {
-                TODO("Not yet implemented")
-            }
-
-            override fun next() {
-                TODO("Not yet implemented")
-            }
-
-        })
+        val viewModel: GameViewModel = GameViewModel(GameRepository.Base())
 
         binding.firstChoiceButton.setOnClickListener {
             val uiState: GameUiState = viewModel.chooseFirst()
@@ -64,7 +49,7 @@ class MainActivity : AppCompatActivity() {
             uiState.update(binding = binding)
         }
 
-        val uiState: GameUiState = viewModel.next()
+        val uiState: GameUiState = viewModel.init()
         uiState.update(binding = binding)
 
 
