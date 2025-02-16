d92794215316af6d685e57ddfb4669ea3d21da74
Sden74 <sitnov.den74@yandex.ru>
2025-02-16 23:24:10 +0500

    QG-14 my navigation without turn screen

--- app/src/main/java/com/example/denquizgame/MainActivity.kt
+++ app/src/main/java/com/example/denquizgame/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.denquizgame.databinding.ActivityMainBinding
 import com.example.denquizgame.databinding.GameOverBinding
 
@@ -14,93 +12,45 @@ class MainActivity() : AppCompatActivity() {
     /*constructor() : super() {
             Log.d("sdv74", "onCreate called")
         }*/
-    init {
-        Log.d("sdv74", "mainActivity")
-    }
+    /* init {
+         Log.d("sdv74", "mainActivity")
+     }*/
 
-    private lateinit var uiState: GameUiState
+    private lateinit var showGameOver: () -> Unit
+    private lateinit var showGame: () -> Unit
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("sdv74", " onCreate ${savedInstanceState == null}")
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        showGame = {
+            GameCustomScreen(showGameOver).show(this, savedInstanceState)
+        }
+        showGameOver = {
+            GameOverMyCustomScreen(showGame).show(this)
+        }
 
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            showGame.invoke()
+        }
 
-        ViewCompat.setOnApplyWindowInsetsListener(/*findViewById(R.id.rootLayout)*/ binding.rootLayout) { v, insets ->
+        /* ViewCompat.setOnApplyWindowInsetsListener(*//*findViewById(R.id.rootLayout)*//* binding.rootLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
+        /*
 
-        //val viewModel: GameViewModel = GameViewModel(GameRepository.Base())
-        // создаем статику модельки GameViewModel
-        // при пересоздании Activity View-моделька не будет пересоздаваться, т.к. находится в статике
-        //
-        //val viewModel: GameViewModel = ViewModelContainer.viewModel
-
-        val viewModel: GameViewModel = (application as QuizApp).viewModel
-
-        val update: () -> Unit = {
-            //uiState.update(binding = binding)
-            uiState.update(
-                binding.questionTextView,
-                binding.firstChoiceButton,
-                binding.secondChoiceButton,
-                binding.thirdChoiceButton,
-                binding.forthChoiceButton,
-                binding.nextButton,
-                binding.checkButton
-            )
-        }
-
-        binding.firstChoiceButton.setOnClickListener {
-            uiState = viewModel.chooseFirst()
-            //uiState.update(binding = binding)
-            update.invoke()
-        }
-        binding.secondChoiceButton.setOnClickListener {
-            uiState = viewModel.chooseSecond()
-            //uiState.update(binding = binding)
-            update.invoke()
-        }
-        binding.thirdChoiceButton.setOnClickListener {
-            uiState = viewModel.chooseThird()
-            //uiState.update(binding = binding)
-            update.invoke()
-        }
-        binding.forthChoiceButton.setOnClickListener {
-            uiState = viewModel.chooseForth()
-            //uiState.update(binding = binding)
-            update.invoke()
-        }
-        binding.checkButton.setOnClickListener {
-            uiState = viewModel.check()
-            //uiState.update(binding = binding)
-            update.invoke()
-        }
-        binding.nextButton.setOnClickListener {
-            /*uiState = viewModel.next()
-            //uiState.update(binding = binding)
-            update.invoke()*/
-            val gameOverBinding: GameOverBinding = GameOverBinding.inflate(layoutInflater)
-            setContentView(gameOverBinding.root)
-            gameOverBinding.newGameButton.setOnClickListener {
-                setContentView(binding.root)
-                uiState = viewModel.init(true)
-                update.invoke()
-            }
-        }
-
-        uiState = viewModel.init(savedInstanceState == null)
-        //uiState.update(binding = binding)
+                //val viewModel: GameViewModel = GameViewModel(GameRepository.Base())
+                // создаем статику модельки GameViewModel
+                // при пересоздании Activity View-моделька не будет пересоздаваться, т.к. находится в статике
+                //
+                //val viewModel: GameViewModel = ViewModelContainer.viewModel
 
 
-        //uiState.update(binding = binding)
-        update.invoke()
 
+        */
 
     }
 
@@ -120,6 +70,7 @@ class MainActivity() : AppCompatActivity() {
         private const val KEY = "uiState"
     }*/
 }
+/*
 
 // Нужен контейнер типа object потому что object не может принимать в себя зависимости
 // и создать object viewModel : GameViewModel(GameRepository.Base())
@@ -127,6 +78,7 @@ class MainActivity() : AppCompatActivity() {
 // НО!!!!! Согласно логам ViewModelContainer создается после Activity что нас не устраивает,
 // поэтому мы от него откажемся
 // в андроид разработке нет смысла использовать синглтоны (object)
+*/
 
 
 /*
@@ -195,3 +147,91 @@ class Init{
     }
 
 }*/
+
+
+class GameCustomScreen(private val showGameOver: () -> Unit) {
+
+
+    fun show(activity: MainActivity, savedInstanceState: Bundle?) {
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(activity.layoutInflater)
+        activity.setContentView(binding.root)
+
+        val viewModel: GameViewModel = (activity.application as QuizApp).gameViewModel
+        lateinit var uiState: GameUiState
+        val update: () -> Unit = {
+            //uiState.update(binding = binding)
+            uiState.update(
+                binding.questionTextView,
+                binding.firstChoiceButton,
+                binding.secondChoiceButton,
+                binding.thirdChoiceButton,
+                binding.forthChoiceButton,
+                binding.nextButton,
+                binding.checkButton
+            )
+        }
+
+        binding.firstChoiceButton.setOnClickListener {
+            uiState = viewModel.chooseFirst()
+            //uiState.update(binding = binding)
+            update.invoke()
+        }
+        binding.secondChoiceButton.setOnClickListener {
+            uiState = viewModel.chooseSecond()
+            //uiState.update(binding = binding)
+            update.invoke()
+        }
+        binding.thirdChoiceButton.setOnClickListener {
+            uiState = viewModel.chooseThird()
+            //uiState.update(binding = binding)
+            update.invoke()
+        }
+        binding.forthChoiceButton.setOnClickListener {
+            uiState = viewModel.chooseForth()
+            //uiState.update(binding = binding)
+            update.invoke()
+        }
+        binding.checkButton.setOnClickListener {
+            uiState = viewModel.check()
+            //uiState.update(binding = binding)
+            update.invoke()
+        }
+
+        /*val showGameFragment = {
+            show(activity,savedInstanceState)
+        }*/
+
+        binding.nextButton.setOnClickListener {
+            //GameOverFragment(showGameFragment).show(activity)
+            showGameOver.invoke()
+            /*uiState = viewModel.next()
+            //uiState.update(binding = binding)
+            update.invoke()*/
+
+        }
+
+        uiState = viewModel.init(savedInstanceState == null)
+        //uiState.update(binding = binding)
+
+
+        //uiState.update(binding = binding)
+        update.invoke()
+    }
+}
+
+class GameOverMyCustomScreen(private val showGameFragment: () -> Unit) {
+    fun show(activity: MainActivity) {
+
+        val viewModel: GameOverViewModel = (activity.application as QuizApp).gameOverViewModel
+
+        val gameOverBinding: GameOverBinding = GameOverBinding.inflate(activity.layoutInflater)
+        activity.setContentView(gameOverBinding.root)
+
+        gameOverBinding.statsTextView.updateOuter(viewModel.statsUiState)
+
+        gameOverBinding.newGameButton.setOnClickListener {
+            //activity.setContentView(binding.root)
+            showGameFragment.invoke()
+        }
+    }
+}
--- app/src/main/java/com/example/denquizgame/QuizApp.kt
+++ app/src/main/java/com/example/denquizgame/QuizApp.kt
@@ -3,6 +3,7 @@ package com.example.denquizgame
 import android.app.Application
 import android.content.Context
 import android.util.Log
+import com.example.denquizgame.views.stats.StatsUiState
 
 class QuizApp : Application() {
     //здесь создавать view-модельку нельзя, т.к. если репе будет нужен контекст то
@@ -11,7 +12,8 @@ class QuizApp : Application() {
 
     //val viewModel = GameViewModel(GameRepository.Base())
 
-    lateinit var viewModel: GameViewModel
+    lateinit var gameViewModel: GameViewModel
+    lateinit var gameOverViewModel: GameOverViewModel
 
     init {
         Log.d("sdv74", "QuizApp")
@@ -21,11 +23,17 @@ class QuizApp : Application() {
         super.onCreate()
         val sharedPreferences = getSharedPreferences("quizAppData", Context.MODE_PRIVATE)
         Log.d("sdv74", "onCreate QuizApp")
-        viewModel = GameViewModel(
+        gameViewModel = GameViewModel(
             GameRepository.Base(
                 IntCache.Base(sharedPreferences, "indexKey", 0),
                 IntCache.Base(sharedPreferences, "userChoiceIndexKey", -1),
             )
         )
+        gameOverViewModel = GameOverViewModel()
     }
+}
+
+class GameOverViewModel {
+    //todo
+    val statsUiState: StatsUiState = StatsUiState.Base(1, 1)
 }
